163a64ba1bdd243037175b1f566e1c1cde44d8c0
Mafiozy001 <andrejtkacenko0531@gmail.com>
2022-06-22 03:00:01 +0400

    main

---
+++ hay/Bilet_7,17,19.py
@@ -0,0 +1,16 @@
+#a = int(input("1: "))
+#b = int(input("2: "))
+#c = int(input("3: "))
+#d = int(input("4: "))
+#e = int(input("5: "))
+#g = int(input("6: "))
+#one = [a,b,c,d,e,g]
+one = [i for i in range(1, 16)]#(1,46)
+class poryd:
+    print(one)
+class neporyd:
+    one.reverse()
+    print(one)
+if not one:
+	print("список пуст!")
+    
