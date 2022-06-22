4c05344c8abddf583a6e93aad95a17eccd91537f
Mafiozy001 <andrejtkacenko0531@gmail.com>
2022-06-22 04:58:25 +0400

    main

---
+++ hay/Bilet_21.py
@@ -0,0 +1,19 @@
+
+q = int(input("Введите число  q:"))
+w = int(input("Введите число  w:"))
+e = int(input("Введите число  e:"))
+r = int(input("Введите число  r:"))
+t = int(input("Введите число  t:"))
+y = int(input("Введите число  y:"))
+u = int(input("Введите число  u:"))
+i = int(input("Введите число  i:"))
+a = [q, w, e, r, t, y, u, i, ]
+max_number = max(a)
+print("Наибольшее число:", max_number)
+
+
+min_number = min(a)
+print("Наименьшее число:", min_number)
+#Без порядкового номера(((
+
+
